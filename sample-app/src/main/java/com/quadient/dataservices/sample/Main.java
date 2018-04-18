7b8d3903dbf8d70df2c5105c2fda04d183762a50
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2018-04-17 19:01:03 -0700

    Fix swagger-codegen extends bug. Sample to do Country Standardization.

--- sample-app/src/main/java/com/quadient/dataservices/sample/Main.java
+++ sample-app/src/main/java/com/quadient/dataservices/sample/Main.java
@@ -1,18 +1,8 @@
 package com.quadient.dataservices.sample;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import com.quadient.dataservices.ClientFactory;
-import com.quadient.dataservices.address.AddressCorrectionRequest;
-import com.quadient.dataservices.address.model.CorrectionRequestAddress;
-import com.quadient.dataservices.address.model.CorrectionResponse;
-import com.quadient.dataservices.address.model.CorrectionResponseAddress;
-import com.quadient.dataservices.address.model.CorrectionResponseRecord;
 import com.quadient.dataservices.api.AdministrativeCredentials;
 import com.quadient.dataservices.api.Client;
 import com.quadient.dataservices.api.Credentials;
@@ -20,6 +10,8 @@ import com.quadient.dataservices.api.HasBaseUri;
 import com.quadient.dataservices.api.JobSession;
 import com.quadient.dataservices.api.QuadientCloudCredentials;
 import com.quadient.dataservices.api.Region;
+import com.quadient.dataservices.country.CountryStandardizationRequest;
+import com.quadient.dataservices.country.model.CountryStandardizationResponse;
 
 public class Main {
 
@@ -43,21 +35,21 @@ public class Main {
 
         final Client client = ClientFactory.createClient(credentials);
 
-        CorrectionResponse response;
+        CountryStandardizationResponse response;
 
-        // call address correction without a job session
-        response = client.execute(new AddressCorrectionRequest(getSampleAddresses()));
-        response.getAddresses().forEach(addr -> {
-            System.out.println("Got response address: " + toOutput(addr));
+        // call country standardization without a job session
+        response = client.execute(new CountryStandardizationRequest("United States", "United Kingdom"));
+        response.getCountries().forEach(c -> {
+            System.out.println("Got response country: " + c.getCountry().getIso2());
         });
 
-        // create a job session and call address correction
+        // create a job session and call country standardization
         final JobSession job = client.createJob();
         System.out.println("Created job: " + job.getJobId());
 
-        response = job.execute(new AddressCorrectionRequest(getSampleAddresses()));
-        response.getAddresses().forEach(addr -> {
-            System.out.println("Got response address: " + toOutput(addr));
+        response = client.execute(new CountryStandardizationRequest("Danmark", "Holland"));
+        response.getCountries().forEach(c -> {
+            System.out.println("Got response country: " + c.getCountry().getIso2());
         });
 
         job.close();
@@ -66,39 +58,5 @@ public class Main {
 
     public static HasBaseUri getRegion(String argument) {
         return argument.length() == 2 ? Region.valueOf(argument) : () -> URI.create(argument);
-    }
-
-    private static String toOutput(CorrectionResponseRecord record) {
-        final StringBuilder sb = new StringBuilder();
-        sb.append(record.getOutcome().getCategory());
-        sb.append(" (");
-        final Set<String> codes = record.getOutcome().getCodes().keySet();
-        sb.append(codes.stream().collect(Collectors.joining(",")));
-        sb.append("): ");
-        final CorrectionResponseAddress address = record.getAddress();
-        if (address.getOrganization() != null && !address.getOrganization().isEmpty()) {
-            sb.append(address.getOrganization() + ". ");
-        }
-        address.getAddressLines().forEach(line -> {
-            sb.append(line + ". ");
-        });
-        address.getRegionalLines().forEach(line -> {
-            sb.append(line + ". ");
-        });
-        sb.append(address.getCountry());
-        return sb.toString();
-    }
-
-    private static List<CorrectionRequestAddress> getSampleAddresses() {
-        final CorrectionRequestAddress address = new CorrectionRequestAddress();
-        address.setAddressLines(Arrays.asList("1301 5th Ave", "Suite 2200"));
-        address.setProvinceOrState("WA");
-        address.setCity("Seattle");
-        address.setPostalCode("98101");
-        address.setCountry("US");
-
-        final List<CorrectionRequestAddress> addresses = new ArrayList<>();
-        addresses.add(address);
-        return addresses;
     }
 }
